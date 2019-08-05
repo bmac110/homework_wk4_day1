280bf24e9aab4363b429f246fd592f35d8cac453
Billy Mackay <bossmack50@gmail.com>
2019-08-05 16:55:04 +0100

    added ERB view file

--- controller.rb
+++ controller.rb
@@ -4,8 +4,12 @@ require("sinatra/contrib/all") if development?
 require_relative("./models/game.rb")
 also_reload("./models/*")
 
-get "play/:hand1/:hand2" do
-  game = Game.new(params[:hand1], params[:hand2])
-  @play = game.play()
+get "/" do
+  erb(:home)
+end
+
+get "/play/:hand1/:hand2" do
+  game = Game.new()
+  @play = game.play(params[:hand1], params[:hand2])
   erb(:result)
 end
--- models/game.rb
+++ models/game.rb
@@ -7,11 +7,15 @@ class Game
     when hand1 == hand2
       return "Draw!"
     when hand1 == "rock" && hand2 == "scissors"
+      return "#{hand1} wins"
     when hand1 == "scissors" && hand2 == "paper"
+      return "#{hand1} wins"
     when hand1 == "paper" && hand2 == "rock"
       return "#{hand1} wins"
     when hand1 == "rock" && hand2 == "paper"
+      return "#{hand2} wins"
     when hand1 == "scissors" && hand2 == "rock"
+      return "#{hand2} wins"
     when hand1 == "paper" && hand2 == "scissors"
       return "#{hand2} wins"
     end
