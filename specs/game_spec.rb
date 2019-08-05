63f1d822dfd27e10dc9718ee30e3b9c58f9c494a
Billy Mackay <bossmack50@gmail.com>
2019-08-05 17:43:18 +0100

    added some style.css

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -7,8 +7,16 @@ class TestGame < MiniTest::Test
     @game = Game.new()
   end
 
-  def test_play()
+  def test_draw()
     assert_equal("Draw!", @game.play("rock", "rock"))
+  end
+
+  def test_hand1_win()
+    assert_equal("rock wins", @game.play("rock", "scissors"))
+  end
+
+  def test_hand2_win()
+    assert_equal("scissors wins", @game.play("paper", "scissors"))
   end
 
 end
